6244bc4cf17b64161a4ab801809078c8d810be9d
mdsaif <Saif.Raza@gov.bc.ca>
2025-12-22 21:05:33 +0530

    Added code for FTP server setup

---
+++ backend/src/ftp/dto/outbound.file.dto.ts
@@ -0,0 +1,19 @@
+import { IsArray, IsNotEmpty, IsString } from 'class-validator';
+
+export class CreateFileDto {
+  @IsString()
+  @IsNotEmpty()
+  system: string;
+
+  @IsString()
+  @IsNotEmpty()
+  flow: string;
+
+  @IsString()
+  @IsNotEmpty()
+  fileType: string;
+
+  @IsArray()
+  @IsNotEmpty()
+  payload: any[];
+}
