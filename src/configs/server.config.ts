df03807b76d6a692e47af10ba8aba23040a0fe41
Raza <mdsaraza@deloitte.com>
2026-02-19 16:30:23 +0530

    fix lint issue

--- src/configs/server.config.ts
+++ src/configs/server.config.ts
@@ -1,5 +1,10 @@
 import { Logger } from '@nestjs/common'
-const { INBOUND_DIR, OUTBOUND_DIR, LOCAL_STORAGE_DIR, CRA_PUB_CERT_PATH='/certs/cra-pub-cert.pem' } = process.env
+const {
+  INBOUND_DIR,
+  OUTBOUND_DIR,
+  LOCAL_STORAGE_DIR,
+  CRA_PUB_CERT_PATH = '/certs/cra-pub-cert.pem',
+} = process.env
 
 const logger = new Logger('FTP-CONFIG')
 
